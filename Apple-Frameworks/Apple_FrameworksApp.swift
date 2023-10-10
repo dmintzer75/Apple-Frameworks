31f5db334a7e31fec5cd26b9eef6881526e3dccc
Dario Mintzer <dmintzer75@hotmail.com>
2023-10-10 12:14:31 -0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by WKS-MAC01 on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
